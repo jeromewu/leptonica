f7533188f9d69640d210085498897b29242afe9a
Jerome Wu <jeromewus@gmail.com>
2018-11-13 07:43:58 +0800

    Add libtiff

--- build-js.sh
+++ build-js.sh
@@ -3,8 +3,27 @@
 # This script is for testing build using emscripten
 #
 
-rm -rf build
-mkdir build
-cd build
-emmake cmake ..
-emmake make
+build_libtiff() {
+  cd libtiff
+  emconfigure ./configure --prefix=${PWD}/usr
+  emmake make install -j4
+  cd ..
+}
+
+build_leptonica() {
+  mkdir build
+  cd build
+  emmake cmake .. \
+    -DTIFF_LIBRARY=libtiff/usr/lib/libtiff.a \
+    -DTIFF_INCLUDE_DIR=libtiff/usr/include \
+    -DCMAKE_INSTALL_PREFIX=../usr \
+    -DSTATIC=ON
+  emmake make install -j4
+}
+
+main() {
+  build_libtiff
+  build_leptonica
+}
+
+main "$@"
