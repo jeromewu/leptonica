873af628ff0505675e91afc6eb00f2cd0dc85c95
Jerome Wu <jeromewus@gmail.com>
2018-11-14 12:40:13 +0800

    Add libpng

--- build-js.sh
+++ build-js.sh
@@ -3,16 +3,26 @@
 # This script is for testing build using emscripten
 #
 
+export CPPFLAGS="-I${PWD}/usr/include"
+export LDFLAGS="-L${PWD}/usr/lib"
+
 build_zlib() {
   cd zlib
-  emconfigure ./configure --prefix=${PWD}/usr
+  emconfigure ./configure --prefix=${PWD}/../usr
+  emmake make install -j4
+  cd ..
+}
+
+build_libpng() {
+  cd libpng
+  emconfigure ./configure --prefix=${PWD}/../usr
   emmake make install -j4
   cd ..
 }
 
 build_libtiff() {
   cd libtiff
-  emconfigure ./configure --prefix=${PWD}/usr
+  emconfigure ./configure --prefix=${PWD}/../usr
   emmake make install -j4
   cd ..
 }
@@ -22,13 +32,13 @@ build_leptonica() {
   mkdir -p build
   cd build
   emmake cmake .. \
-    -DCMAKE_INSTALL_PREFIX=../usr \
-    -DSTATIC=ON
+    -DCMAKE_INSTALL_PREFIX=../usr
   emmake make install -j4
 }
 
 main() {
   build_zlib
+  build_libpng
   build_libtiff
   build_leptonica
 }
